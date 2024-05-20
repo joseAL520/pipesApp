979ca4215a2f340ccd10121a13c10a1fd821d293
jose angel <jose.albelt1817@gmail.com>
2024-05-20 15:53:19 -0500

    pipes i18select

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+
+  // i18 select
+  public name:string = 'jose';
+  public gender: 'male' | 'female' = 'male';
+
+  public invitarloMap = { 
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+
+
+  changeClient(){
+    this.name = 'melissa';
+    this.gender = 'female';
+  }
 }
